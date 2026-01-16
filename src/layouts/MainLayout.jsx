510c3b0912071aa650de048d0f589dab92f543a7
Bruno_Lopes <brunoviniciuslopes2004@gmail.com>
2026-01-15 23:06:08 -0300

    refactor(layout):otimizar styles

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
-
+import Footer from "../components/Footer"; 
+import styles from "./MainLayout.module.css"
 export default function MainLayout({ children }) {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
       <main>{children}</main>
-      
-    </>
+      <Footer></Footer>
+    </div>
   );
 }
